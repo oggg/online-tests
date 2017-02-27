2e6a5b828c632bb7e40b803524afd22ba2862b0e
oggg <ogggster@gmail.com>
2017-02-27 22:10:56 +0200

    Updated account controller to reflect the username login

--- OnlineTest.Source/Web/OnlineTest.Web/Controllers/AccountController.cs
+++ OnlineTest.Source/Web/OnlineTest.Web/Controllers/AccountController.cs
@@ -80,7 +80,7 @@ namespace OnlineTest.Web.Controllers
 
             // This doesn't count login failures towards account lockout
             // To enable password failures to trigger account lockout, change to shouldLockout: true
-            var result = await SignInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, shouldLockout: false);
+            var result = await SignInManager.PasswordSignInAsync(model.UserName, model.Password, model.RememberMe, shouldLockout: false);
             switch (result)
             {
                 case SignInStatus.Success:
@@ -168,7 +168,7 @@ namespace OnlineTest.Web.Controllers
                     // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
                     // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
 
-                    return RedirectToAction("Index", "Home");
+                    return RedirectToAction("Index", "Tests");
                 }
                 AddErrors(result);
             }
